d2bbcb8a81ede45a81bbb8330905c37352786ca7
Vishnu Iyer <vishnu72498@gmail.com>
2016-11-12 13:07:05 -0800

    changed some things

--- main/login/views.py
+++ main/login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from . import models
 # Create your views here.
 def login_view(request):
@@ -12,16 +13,21 @@ def create_user(request):
     p = request.POST
     ps, psc = p.get('password', ''), p.get('password_confirm', '')
     if ps != psc:
-        return render(request, 'pages/signup.html', {'error_message': 'Passwords do not match'})
-    user = User()
-    user.username = p.get('username', '')
-    user.first_name = p.get('first_name', '')
-    user.last_name = p.get('last_name', '')
-    user.email = p.get('email', '')
-    user.password = ps
-    user.save()
-    member = models.Member(user=user)
-    member.hometown = p.get('hometown', '')
-    member.profile_pic = p.get('profile_pic', None)
-    member.save()
+        return render(request, 'pages/signup.html', {'error_message': 'Passwords do not match.'})
+    try:
+        user = User()
+        user.username = p.get('username', '')
+        user.first_name = p.get('first_name', '')
+        user.last_name = p.get('last_name', '')
+        user.email = p.get('email', '')
+        user.password = ps
+        if len(ps) < 8:
+            return render(request, 'pages/signup.html', {'error_message': 'That password is too short. Please make it 8 characters or longer.'})
+        user.save()
+        member = models.Member(user=user)
+        member.hometown = p.get('hometown', '')
+        member.profile_pic = p.get('profile_pic', None)
+        member.save()
+    except IntegrityError:
+        return render(request, 'pages/signup.html', {'error_message': 'There is already a user with username:' + p.get('username', '') +'.'})
     return render(request, 'pages/success.html')
