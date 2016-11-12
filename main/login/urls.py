5e3ba2d7c4398757aecacbb85f8e0a21847c7b30
Vishnu Iyer <vishnu72498@gmail.com>
2016-11-12 12:46:13 -0800

    working login page

--- main/login/urls.py
+++ main/login/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'login'
 urlpatterns = [
-    url(r'^new-login/', views.signup_view, name='signup'),
-    url(r'^create/', views.create_user, name='create'),
-    url(r'^$', views.login_view, name='index'),
+    url(r'^create/$', views.signup_view, name='create'),
+    url(r'^new-user/', views.create_user, name='new_user'),
+    url(r'^$', views.login_view, name='login'),
 ]
--- main/login/views.py
+++ main/login/views.py
@@ -13,13 +13,15 @@ def create_user(request):
     ps, psc = p.get('password', ''), p.get('password_confirm', '')
     if ps != psc:
         return render(request, 'pages/signup.html', {'error_message': 'Passwords do not match'})
-    member = models.Member(user = User())
-    user = member.user
+    user = User()
     user.username = p.get('username', '')
     user.first_name = p.get('first_name', '')
     user.last_name = p.get('last_name', '')
     user.email = p.get('email', '')
     user.password = ps
+    user.save()
+    member = models.Member(user=user)
     member.hometown = p.get('hometown', '')
     member.profile_pic = p.get('profile_pic', None)
+    member.save()
     return render(request, 'pages/success.html')
