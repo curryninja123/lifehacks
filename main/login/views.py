f128c55dfae7417e458b29cd61fdc06ddfddd2f9
Jeremy <zhesah@gmail.com>
2016-11-12 04:15:15 -0800

    added views

--- main/login/views.py
+++ main/login/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 # Create your views here.
-def index(request):
+def login_view(request):
     return render(request, 'pages/login.html')
+
+def signup_view(request):
+	return render(request, 'pages/signup.html')
