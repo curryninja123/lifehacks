88827b20aa0e89225e3f6a5b19d180b08d466cf8
Vishnu Iyer <vishnu72498@gmail.com>
2016-11-12 04:21:00 -0800

    created add comment method

--- main/hack/views.py
+++ main/hack/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 def index(request):
     return render(request, 'pages/create.html')
+
+def add_comment(wisdom, comment):
+    wisdom.comment_list.append(comment)
+
+def 
