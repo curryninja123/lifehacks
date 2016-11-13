463168b2bb11bf4395aa6b3db58d6d53382ce473
Vishnu Iyer <vishnu72498@gmail.com>
2016-11-12 18:54:27 -0800

    created logout function

--- main/home/views.py
+++ main/home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth import logout
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html', {})
 
 def categories(request):
 	return render(request, 'pages/categories.html', {})
+def logout_user(request):
+    logout(request)
+    return HttpResponse('You have been usccessfully logged out.')
