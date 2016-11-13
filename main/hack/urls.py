1e6fe00c49dee4baf80310ef1765658a5b504866
Vishnu Iyer <vishnu72498@gmail.com>
2016-11-12 19:32:34 -0800

    reorganized primary key search

--- main/hack/urls.py
+++ main/hack/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^new_tip/', views.new_tip, name='new_tip'),
     url(r'^post_hack/', views.post_hack, name='post_hack'),
     url(r'^post_tip/', views.post_tip, name='post_tip'),
-    url(r'^view_hack/(?P<hack_id>[0-9]+)', views.display_hack, name='display_hack'),
-    url(r'^view_tip/(?P<tip_id>[0-9]+)', views.display_tip, name='display_tip'),
+    url(r'^view_hack/primary/(?P<hack_id>[0-9]+)', views.display_hack, name='display_hack'),
+    url(r'^view_tip/primary/(?P<tip_id>[0-9]+)', views.display_tip, name='display_tip'),
     url(r'^$', views.index, name='index'),
 ]
